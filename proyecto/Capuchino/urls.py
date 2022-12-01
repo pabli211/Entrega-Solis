d2fd3efc0568d24dedac758cbf7456249796fbcd
unknown <you@example.com>
2022-12-01 20:40:50 -0300

    Capuchino version 3.0

--- proyecto/Capuchino/urls.py
+++ proyecto/Capuchino/urls.py
@@ -2,21 +2,38 @@ from django.urls import path
 from Capuchino.views import *
 
 urlpatterns = [
-    path('recetas/', receta, name='receta'),
-    path('cheffs/', cheff, name='cheff'),
-    path('clientes/', cliente, name='cliente'),
-    path('restaurants/', restaurant, name='restaurant'),
     path('inicio/', inicio, name='inicio'),
 
+    #---------------------Receta:
+    path('recetas/', receta, name='receta'),
     path('busquedareceta/', buscarReceta, name='buscarReceta'),
     path('busqueda-receta/', busquedaReceta, name='buscar_receta'),
+    path('leerReceta/', leerReceta, name='leerReceta'),
+    path('eliminarReceta/<id>', eliminarReceta, name='eliminarReceta'),
 
-    path('busquedarest/', buscarRest, name='buscarRest'),
-    path('busqueda-restaurant/', busquedaRest, name='buscar_rest'),
 
+    #----------------------Cheff:
+    path('cheffs/', cheff, name='cheff'),
     path('busquedachef/', buscarChef, name='buscarChef'),
     path('busqueda-chef/', busquedaChef, name='buscar_chef'),
+    path('leerChefs/', leerChef, name='leerChef'),
+    path('eliminarChef/<id>', eliminarChef, name='eliminarChef'),
+    path('editarChef/<id>', editarChef, name='editarChef'),
 
+
+    #----------------------Cliente:
+    path('clientes/', cliente, name='cliente'),
     path('busquedacliente/', buscarCliente, name='buscarCliente'),
     path('busqueda-cliente/', busquedaCliente, name='buscar_cliente'),
+    path('leerCliente/', leerCliente, name='leerCliente'),
+    path('eliminarCliente/<id>', eliminarCliente, name='eliminarCliente'),
+
+
+    #---------------------Restaurants:   
+    path('restaurants/', restaurant, name='restaurant'),
+    path('busquedarest/', buscarRest, name='buscarRest'),
+    path('busqueda-restaurant/', busquedaRest, name='buscar_rest'),
+    path('leerRestaurant/', leerRest, name='leerRest'),
+    path('eliminarRestaurant/<id>', eliminarRest, name='eliminarRest'),
+
 ]
--- proyecto/Capuchino/views.py
+++ proyecto/Capuchino/views.py
@@ -7,62 +7,9 @@ from Capuchino.forms import *
 def inicio(request):
     return render(request, 'Capuchino/inicio.html')
 
-def cliente(request):
-    if request.method == "POST":
-        form= ClienteForm(request.POST)
-        if form.is_valid():
-            informacion= form.cleaned_data
 
-            nombre= informacion['nombre']
-            apellido= informacion['apellido']
-            email= informacion['email']
-            reseña= informacion['reseña']
 
-            cliente= Cliente(nombre=nombre, apellido=apellido, email=email, reseña=reseña)
-            cliente.save()
-            return render(request,'Capuchino/inicio.html', {'mensaje':'Éxito al enviar la reseña'})
-    else:
-        formulario= ClienteForm()
-    
-    return render(request, 'Capuchino/clientes.html', {"form":formulario},)
-
-def cheff(request):
-    if request.method == "POST":
-        form= CheffForm(request.POST)
-        if form.is_valid():
-            informacion= form.cleaned_data
-
-            nombre= informacion['nombre']
-            apellido= informacion['apellido']
-            email= informacion['email']
-            restaurant= informacion['restaurant']
-            especialidad= informacion['especialidad']
-
-            cheff= Cheff(nombre=nombre, apellido=apellido, email=email, restaurant=restaurant, especialidad=especialidad)
-            cheff.save()
-            return render(request,'Capuchino/inicio.html', {'mensaje':'Chef creado correctamente'})
-    else:
-        formulario= CheffForm()
-    
-    return render(request, 'Capuchino/cheffs.html', {"form":formulario},)
-
-def restaurant(request):
-    if request.method == "POST":
-        form= RestaurantForm(request.POST)
-        if form.is_valid():
-            informacion= form.cleaned_data
-
-            nombre= informacion['nombre']
-            direccion= informacion['direccion']
-            estrellas= informacion['estrellas']
-
-            restaurant= Restaurant(nombre=nombre, direccion=direccion, estrellas=estrellas)
-            restaurant.save()
-            return render(request,'Capuchino/inicio.html', {'mensaje':'Restaurant añadido correctamente'})
-    else:
-        formulario= RestaurantForm()
-    
-    return render(request, 'Capuchino/restaurants.html', {"form":formulario},)
+#------------------------Bloque Recetas
 
 def receta(request):
     if request.method == "POST":
@@ -85,7 +32,6 @@ def receta(request):
 def buscarReceta(request):
     return render(request, 'Capuchino/buscarReceta.html')
 
-
 def busquedaReceta(request):
 
     if request.GET["nombre"]:
@@ -97,9 +43,103 @@ def busquedaReceta(request):
     else:
         return render(request, 'Capuchino/buscarReceta.html', {'mensaje_2':'por favor ingrese una receta'})
 
+def leerReceta(request):
+    receta= Receta.objects.all()
+    return render(request, 'Capuchino/leerReceta.html', {'recetas':receta})
 
+def eliminarReceta(request, id):
+    receta= Receta.objects.get(id=id)
+    receta.delete()
 
+    receta= Receta.objects.all()
+    return render(request, 'Capuchino/leerReceta.html', {'mensaje':'Receta eliminado correctamente', 'recetas':receta})
 
+#------------------------Bloque Cheff
+
+def cheff(request):
+    if request.method == "POST":
+        form= CheffForm(request.POST)
+        if form.is_valid():
+            informacion= form.cleaned_data
+
+            nombre= informacion['nombre']
+            apellido= informacion['apellido']
+            email= informacion['email']
+            restaurant= informacion['restaurant']
+            especialidad= informacion['especialidad']
+
+            cheff= Cheff(nombre=nombre, apellido=apellido, email=email, restaurant=restaurant, especialidad=especialidad)
+            cheff.save()
+            return render(request,'Capuchino/inicio.html', {'mensaje':'Chef creado correctamente'})
+    else:
+        formulario= CheffForm()
+    
+    return render(request, 'Capuchino/cheffs.html', {"form":formulario},)
+
+def buscarChef(request):
+    return render(request, 'Capuchino/buscarChef.html')
+
+def busquedaChef(request):
+
+    if request.GET["nombre"]:
+
+        nombre= request.GET["nombre"]
+        chefs= Cheff.objects.filter(nombre__icontains=nombre)
+        return render(request, 'Capuchino/resultadoChef.html', {'chefs':chefs})
+
+    else:
+        return render(request, 'Capuchino/buscarChef.html', {'mensaje_2':'por favor ingrese un Chef'})
+
+def leerChef(request):
+    chef= Cheff.objects.all()
+    return render(request, 'Capuchino/leerChef.html', {'chefs':chef})
+
+def eliminarChef(request, id):
+    chef= Cheff.objects.get(id=id)
+    chef.delete()
+
+    chef= Cheff.objects.all()
+    return render(request, 'Capuchino/leerChef.html', {'mensaje':'Chef eliminado correctamente', 'chefs':chef})
+
+def editarChef(request, id):
+    chef= Cheff.objects.get(id=id)
+    if request.method=='POST':
+        form= CheffForm(request.POST)
+        if form.is_valid():
+            informacion= form.cleaned_data
+            chef.nombre= informacion['nombre']
+            chef.apellido= informacion['apellido']
+            chef.email= informacion['email']
+            chef.restaurant= informacion['restaurant']
+            chef.especialidad= informacion['especialidad']
+
+            chef.save()
+            chef= Cheff.objects.all()
+            return render(request, 'Capuchino/leerChef.html', {'mensaje':'Chef editado correctamente', 'chefs':chef})
+            
+    else:
+        formulario= CheffForm(initial={'nombre':chef.nombre , 'apellido':chef.apellido, 'email':chef.email, 'restaurant':chef.restaurant, 'especialidad':chef.especialidad})
+    return render(request, 'Capuchino/editarChef.html', {'form':formulario, 'chefs':chef})
+
+#------------------------Bloque Restaurats
+
+def restaurant(request):
+    if request.method == "POST":
+        form= RestaurantForm(request.POST)
+        if form.is_valid():
+            informacion= form.cleaned_data
+
+            nombre= informacion['nombre']
+            direccion= informacion['direccion']
+            estrellas= informacion['estrellas']
+
+            restaurant= Restaurant(nombre=nombre, direccion=direccion, estrellas=estrellas)
+            restaurant.save()
+            return render(request,'Capuchino/inicio.html', {'mensaje':'Restaurant añadido correctamente'})
+    else:
+        formulario= RestaurantForm()
+    
+    return render(request, 'Capuchino/restaurants.html', {"form":formulario},)
 
 def buscarRest(request):
     return render(request, 'Capuchino/buscarRest.html')
@@ -115,35 +155,40 @@ def busquedaRest(request):
     else:
         return render(request, 'Capuchino/buscarRest.html', {'mensaje_2':'por favor ingrese un restaurant'})
 
+def leerRest(request):
+    rest= Restaurant.objects.all()
+    return render(request, 'Capuchino/leerRest.html', {'rests':rest})
 
+def eliminarRest(request, id):
+    rest= Restaurant.objects.get(id=id)
+    rest.delete()
 
+    rest= Restaurant.objects.all()
+    return render(request, 'Capuchino/leerRest.html', {'mensaje':'Restaurant eliminado correctamente', 'rests':rest})
 
+#------------------------Bloque Clientes
 
+def cliente(request):
+    if request.method == "POST":
+        form= ClienteForm(request.POST)
+        if form.is_valid():
+            informacion= form.cleaned_data
 
-def buscarChef(request):
-    return render(request, 'Capuchino/buscarChef.html')
+            nombre= informacion['nombre']
+            apellido= informacion['apellido']
+            email= informacion['email']
+            reseña= informacion['reseña']
 
-
-def busquedaChef(request):
-
-    if request.GET["nombre"]:
-
-        nombre= request.GET["nombre"]
-        chefs= Cheff.objects.filter(nombre__icontains=nombre)
-        return render(request, 'Capuchino/resultadoChef.html', {'chefs':chefs})
-
+            cliente= Cliente(nombre=nombre, apellido=apellido, email=email, reseña=reseña)
+            cliente.save()
+            return render(request,'Capuchino/inicio.html', {'mensaje':'Éxito al enviar la reseña'})
     else:
-        return render(request, 'Capuchino/buscarChef.html', {'mensaje_2':'por favor ingrese un Chef'})
-
-
-
-
-
-
+        formulario= ClienteForm()
+    
+    return render(request, 'Capuchino/clientes.html', {"form":formulario},)
 
 def buscarCliente(request):
     return render(request, 'Capuchino/buscarCliente.html')
-
 
 def busquedaCliente(request):
 
@@ -155,3 +200,14 @@ def busquedaCliente(request):
 
     else:
         return render(request, 'Capuchino/buscarCliente.html', {'mensaje_2':'por favor ingrese un usuario'})
+
+def leerCliente(request):
+    cliente= Cliente.objects.all()
+    return render(request, 'Capuchino/leerCliente.html', {'clientes':cliente})
+
+def eliminarCliente(request, id):
+    cliente= Cliente.objects.get(id=id)
+    cliente.delete()
+
+    cliente= Cliente.objects.all()
+    return render(request, 'Capuchino/leerCliente.html', {'mensaje':'Cliente eliminado correctamente', 'clientes':cliente})
